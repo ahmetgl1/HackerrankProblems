1d8ea956c0c9b00e243fc57f6789adc596587910
ahmetgl1 <ahmetfatih.guzeller@pru.edu.tr>
2022-12-01 00:51:53 +0300

    First Question

---
+++ HackerrankAlgorithmQuestion/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+FirstLast firstLast = new FirstLast();
+
+int nums[] = {1 , 2 , 4 , 5};
+
+System.out.println(firstLast.firstLast6(nums));
+
+
+    }
+}
